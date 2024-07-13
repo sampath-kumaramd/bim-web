f891b46f262bbfd015a11f7c7e8b4fe7d5d792c6
Dilini_Liyanage <ldsliyanage99@gmail.com>
2024-07-13 18:29:43 +0530

    completed legal page

---
+++ bin/LegalData.ts
@@ -0,0 +1,32 @@
+type LegalArticle = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export const LegalArticles: LegalArticle[] = [
+  {
+    id: 1,
+    title: 'THE PUBLISHER',
+    description:
+      'The publishing of the Site is ensured by Bim Group SARL-S with capital of 10,000 euros, registered with Luxembourg Trade and Companies Register under number 123456 whose head office is located at 36, route du succes, Telephone number 0427827257, Email address: contact@bim-dating.com. Intracommunity VAT number: 737286369 The Publication Director is Houzet Vincent hereinafter the “Publisher”.',
+  },
+  {
+    id: 2,
+    title: "L'HEBERGEUR",
+    description:
+      'The Site host is the company OVH, whose head office is located at 2, rue Kellermann, 59100Roubaix, with telephone number: 1007 + contact email address',
+  },
+  {
+    id: 3,
+    title: 'ACCESS TO THE SITE',
+    description:
+      'The Site is accessible anywhere, 7 days a week, 24 hours a day except in cases of force majeure or interruption. scheduled or not and which may arise from a need for maintenance. In the event of modification, interruption or suspension of the Site, the Publisher cannot be held responsible.',
+  },
+  {
+    id: 4,
+    title: 'DATA COLLECTION',
+    description:
+      'The Site ensures that the User collects and processes personal information in compliance with privacy in accordance with law n°78-17 of January 6, 1978 relating to computers, files and freedoms. 1 Under the Data Protection Act, dated January 6, 1978, the User has a rightaccess, rectification, deletion and opposition of personal data. The user exercises this right: · by email to the email address contact@bim-dating.com Any use, reproduction, distribution, marketing, modification of all or part of the Site, without authorization from the Publisher is prohibited and may result in legal action and prosecution as provided in particular by the Intellectual Property Code and the Civil Code.',
+  },
+];
