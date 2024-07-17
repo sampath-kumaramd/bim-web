184173d14eb796e6f84c6805b767f72a1da787bb
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-07-18 01:26:13 +0530

    add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Toaster />
         <Footer />
       </body>
     </html>
