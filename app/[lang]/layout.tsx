618c4ca8c5027979281e556e98cfdc86c6c7e1eb
Dilini_Liyanage <ldsliyanage99@gmail.com>
2024-08-05 21:06:45 +0530

    setup google analytics

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
@@ -38,13 +39,35 @@ export default async function RootLayout({
           type="image/jpg"
           href="/favico.png"
         />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         <LanguageProvider>
           <Header />
           {children}
           <CookieConsentDialog />
-          {/* <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_ID!} /> */}
+          <GoogleAnalytics
+            GA_MEASUREMENT_ID={
+              process.env.NEXT_PUBLIC_GA_ID!
+            }
+          />
           <Toaster />
           <Footer />
         </LanguageProvider>
